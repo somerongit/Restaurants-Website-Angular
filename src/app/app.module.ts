ab8acaab7cf689bc8ed0d8b278882e19d0d31c6e
somerongit <someron.bakuli05@gmail.com>
2021-08-11 11:03:28 +0530

    Component Part 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatToolbarModule } from '@angular/material/toolbar'; 
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { AppComponent } from './app.component';
 import 'hammerjs';
 import { MenuComponent } from './menu/menu.component';
-
+import { MatCardModule } from '@angular/material';
+import { MatButtonModule } from '@angular/material';
 import { MatListModule } from '@angular/material/list';
+import { MatGridListModule } from '@angular/material/grid-list';
 
 @NgModule({
   declarations: [
@@ -15,7 +17,10 @@ import { MatListModule } from '@angular/material/list';
     MenuComponent
   ],
   imports: [
+    MatCardModule,
+    MatButtonModule,
     MatListModule,
+    MatGridListModule,
     BrowserModule,
     BrowserAnimationsModule,
     MatToolbarModule,
