e0997a2d4b6af000bfb4543357bfb8b51cdb37ce
somerongit <someron.bakuli05@gmail.com>
2021-08-14 08:39:03 +0530

    Router Added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,14 +11,27 @@ import { MatButtonModule } from '@angular/material';
 import { MatListModule } from '@angular/material/list';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
+import { DishService } from './services/dish.service';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+import { AppRoutingModule } from './app-routing/app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
-    DishdetailComponent
+    DishdetailComponent,
+    HeaderComponent,
+    FooterComponent,
+    AboutComponent,
+    HomeComponent,
+    ContactComponent
   ],
   imports: [
+    AppRoutingModule,
     MatCardModule,
     MatButtonModule,
     MatListModule,
@@ -28,7 +41,9 @@ import { DishdetailComponent } from './dishdetail/dishdetail.component';
     MatToolbarModule,
     FlexLayoutModule
   ],
-  providers: [],
+  providers: [
+    DishService,
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
